a91b939fc6271855aac3cbca08adc374b9141792
DianaGisela <dian.gc4@gmail.com>
2016-05-10 20:02:13 +0200

    Update menu.js

--- ejercicio/assets/js/menu.js
+++ ejercicio/assets/js/menu.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function(){
 
-// al pasar se marcan, despues se remueve la clase y al final aparece el sub menu
+// al hacer click cambia de color
 $('ul:first li a').click(function() {
    $('.current-menu-item').removeClass();
   $(this).find().addClass('current-menu-item');
 
 });
 
-// hover  sobre el submenu efecto
+// hover  sobre el submenu efecto solo pasando
   
   $('ul:first li').hover(function(){
     $(this).find('.sub-menu').fadeToggle(100);
